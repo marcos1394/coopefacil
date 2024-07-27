d3c8180919910f7ca2a8c806ee94379887c34760
Marcos Sandoval <marcosbancaprepa@gmail.com>
2024-07-27 00:55:36 +0000

    mejora usuario 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
@@ -9,9 +9,7 @@ import ReceiptsPage from './pages/ReceiptsPage';
 import ExpensesPage from './pages/ExpensesPage';
 import PaymentsPage from './pages/PaymentsPage';
 import SchoolConfigPage from './pages/SchoolConfigPage';
-import ViewData from './components/ViewData';
-import Confirmation from './components/Confirmation';
-import PaymentCart from './components/PaymentCart';
+import CooperativeDataPage from './pages/CooperativaDataPage';
 import theme from './theme';
 
 function App() {
@@ -20,7 +18,6 @@ function App() {
       <Router basename={process.env.PUBLIC_URL}>
         <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/dashboard" element={<DashboardPage />} />
@@ -28,11 +25,7 @@ function App() {
             <Route path="/expenses" element={<ExpensesPage />} />
             <Route path="/payments" element={<PaymentsPage />} />
             <Route path="/school-config" element={<SchoolConfigPage />} />
-            <Route path="/viewdata" element={<ViewData />} />
-            <Route path="/confirmation" element={<Confirmation />} />
-            <Route path="/payment-cart" element={<PaymentCart />} />
-            <Route path="/cooperative-data" element={<CooperativeData />} />
-            <Route path="/sections" element={<SectionsPage />} />
+            <Route path="/cooperative-data" element={<CooperativeDataPage />} />
           </Routes>
           <Box display="flex" justifyContent="space-between" p={2} bgcolor="#f0f2f5">
             <Button variant="text" color="primary" href="mailto:consulta@coopefacil.com">
